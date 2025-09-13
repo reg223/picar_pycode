962cfc7a12ab54da05c96d4dda4a1551aab37bd6
reg223 <reg22355@gmail.com>
2025-09-13 15:17:49 +0100

    p1 complete?

--- P1/p1.py
+++ P1/p1.py
@@ -18,34 +18,36 @@ music = Music()
 
 def main():
     px.set_dir_servo_angle(0) #straighten the servo
-    
-    while True:
-        dist = round(px.ultrasonic.read(), 2)
-        if dist < SAFE:
-            px.forward(0)
-            music.sound_play_threading('car-double-horn.wav')
-            px.set_dir_servo_angle(20)
-            px.backward(MOVE)
-            time.sleep(0.3)
-            px.set_dir_servo_angle(0)
-            time.sleep(0.3)
-            px.set_dir_servo_angle(-20)
-            time.sleep(0.3)
-            
-            px.set_dir_servo_angle(20)
-            px.forward(MOVE)
-            time.sleep(0.3)
-            px.set_dir_servo_angle(0)
-            time.sleep(0.3)
-            px.set_dir_servo_angle(-20)
-            time.sleep(0.3)
-            px.set_dir_servo_angle(0)
-            px.forward(0)
+    try: 
+        while True:
+            dist = round(px.ultrasonic.read(), 2)
+            if dist < SAFE:
+                px.forward(0)
+                time.sleep(1)
+                music.sound_play_threading('car-double-horn.wav')
+                px.set_dir_servo_angle(30)
+                px.backward(MOVE)
+                time.sleep(0.3)
+                px.set_dir_servo_angle(0)
+                time.sleep(1)
+                px.set_dir_servo_angle(-30)
+                time.sleep(0.3)
+                
+                px.set_dir_servo_angle(30)
+                px.forward(MOVE)
+                time.sleep(0.3)
+                px.set_dir_servo_angle(0)
+                time.sleep(1)
+                px.set_dir_servo_angle(-30)
+                time.sleep(0.3)
+                px.set_dir_servo_angle(0)
+                px.forward(0)
 
-        else:
-            px.forward(MOVE)
-            time.sleep(0.1)
-
+            else:
+                px.forward(MOVE)
+                time.sleep(0.1)
+    finally:
+        px.forward(0)
 
 
 if __name__ == "__main__":
